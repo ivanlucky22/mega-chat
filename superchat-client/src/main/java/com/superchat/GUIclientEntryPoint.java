2ad9877f0e9c01edb26356a348cbda83ab4763f7
badcat301 <badcat301@gmail.com>
2017-07-17 08:35:32 -0400

    Client GUI create. Task #2.

---
+++ superchat-client/src/main/java/com/superchat/GUIclientEntryPoint.java
@@ -0,0 +1,37 @@
+package com.superchat;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ * Created by adm on 17.07.2017.
+ */
+public class GUIclientEntryPoint extends Application
+
+{
+
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+
+
+        fxmlLoader.setLocation(this.getClass().getClassLoader().getResource("fxml/clientGUI.fxml"));
+
+        Pane Pane = (Pane) fxmlLoader.load();
+        Scene scene = new Scene(Pane);
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Chat");
+        primaryStage.show();
+    }
+}
---
+++ superchat-client/src/main/java/controller/ClientChatController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+/**
+ * Created by adm on 17.07.2017.
+ */
+public class ClientChatController {
+    public void contentWindowTextField(ActionEvent actionEvent) {
+
+    }
+
+    public void messageText(ActionEvent actionEvent) {
+
+    }
+
+    public void userListTextField(ActionEvent actionEvent) {
+
+    }
+
+    public void sendMessageButton(ActionEvent actionEvent) {
+
+    }
+
+    public void connectServerButton(ActionEvent actionEvent) {
+
+    }
+
+    public void disconnectServerButton(ActionEvent actionEvent) {
+
+    }
+}
