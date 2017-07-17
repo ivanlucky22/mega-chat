5935a46a373bfd86833b12ce72d1695012970707
badcat301 <badcat301@gmail.com>
2017-07-17 10:02:32 -0400

    Server GUI create. Task #1.

---
+++ superchat-server/src/main/java/controller/ServerChatController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+/**
+ * Created by adm on 17.07.2017.
+ */
+public class ServerChatController {
+    public void startServerBatton(ActionEvent actionEvent) {
+
+    }
+
+    public void connectServerBatton(ActionEvent actionEvent) {
+
+    }
+
+    public void disconnectServerBatton(ActionEvent actionEvent) {
+
+    }
+
+    public void chatLog(ActionEvent actionEvent) {
+
+    }
+}
